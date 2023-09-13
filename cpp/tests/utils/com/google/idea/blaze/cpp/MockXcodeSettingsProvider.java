5109b47910fba85d3b80adf2ab6be4248ffad1df
Gowroji Sunil <sgowroji@google.com>
2023-09-13 14:16:21 +0530

    Update MockXcodeSettingsProvider.java

--- cpp/tests/utils/com/google/idea/blaze/cpp/MockXcodeSettingsProvider.java
+++ cpp/tests/utils/com/google/idea/blaze/cpp/MockXcodeSettingsProvider.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.google.idea.blaze.cpp;
 package com.google.idea.blaze.cpp;
 
 import com.google.idea.blaze.base.scope.BlazeContext;
