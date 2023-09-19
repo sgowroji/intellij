1a5fa2c20a86528c63daf28fbeb5ec3c4e41f9a9
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-09-19 12:39:56 +0530

    Update assignee.js

--- .github/workflows/assignee.js
+++ .github/workflows/assignee.js
@@ -1,5 +1,5 @@
  async ({ github, context }) => {
-            await sleep(1000)
+            await sleep(10000)
             const labels = context.payload.pull_request.labels.map(label => label.name);
             console.log("line 30",labels)
             const assignees = [];
