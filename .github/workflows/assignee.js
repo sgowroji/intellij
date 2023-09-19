b2b65c6d88e4e7e2e731249d4dc2bf19395c5a0f
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-09-19 12:38:24 +0530

    Create assignee.js

---
+++ .github/workflows/assignee.js
@@ -0,0 +1,27 @@
+ async ({ github, context }) => {
+            await sleep(1000)
+            const labels = context.payload.pull_request.labels.map(label => label.name);
+            console.log("line 30",labels)
+            const assignees = [];
+            const labelsToAssignees = {
+              'product: CLion': ['sgowroji'],
+              'product: IntelliJ': ['tilakrayal'],
+              'product: GoLand': ['chunduriv'],
+              'product: Android Studio': ['sgowroji']
+              // Add more label-assignee mappings here
+            };
+            for (const label of labels) {
+              if (label in labelsToAssignees) {
+                assignees.push(...labelsToAssignees[label]);
+              }
+            }
+            if (assignees.length > 0) {
+              const issue_number = context.payload.pull_request.number;
+              await github.issues.addAssignees({
+                issue_number,
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                assignees
+              });
+            }
+            }
