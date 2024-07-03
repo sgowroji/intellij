304b84f0bfa0d9931bfa57f52cef9b31d85c5204
Gowroji Sunil <sgowroji@google.com>
2024-07-03 13:14:35 +0530

    Update BlazeScalaSyncPluginTest.java

--- scala/tests/unittests/com/google/idea/blaze/scala/sync/BlazeScalaSyncPluginTest.java
+++ scala/tests/unittests/com/google/idea/blaze/scala/sync/BlazeScalaSyncPluginTest.java
@@ -40,6 +40,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+// Scala implementation
 /** Unit tests for {@link BlazeScalaSyncPlugin} */
 @RunWith(JUnit4.class)
 public class BlazeScalaSyncPluginTest extends BlazeTestCase {
